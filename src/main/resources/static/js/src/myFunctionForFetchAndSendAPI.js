9e234dedf1a2f86f6d1882d6e29722ffca7fb177
shuvoshahacsedu22 <shuvoshaha7@gmail.com>
2020-03-23 15:53:51 +0600

    Refactoring only to the javascript file fetchdelete...

--- src/main/resources/static/js/src/myFunctionForFetchAndSendAPI.js
+++ src/main/resources/static/js/src/myFunctionForFetchAndSendAPI.js
@@ -7,25 +7,9 @@ function fetchData(stat) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(id==='0'){
-            document.getElementById('myForms_employeeUpdateForm_employeeName').setAttribute('value','');
-            //document.getElementById('myForms_employeeUpdateForm_employeeName').setAttribute('placeholder',employee.employeeName);
-            document.getElementById('myForms_employeeUpdateForm_employeeEmail').setAttribute('value','');
-            document.getElementById('myForms_employeeUpdateForm_departmentName').setAttribute('value','');
-            document.getElementById('myForms_employeeUpdateForm_departmentId').setAttribute('value','');
-            document.getElementById('myForms_employeeUpdateForm_joiningDate').setAttribute('value','');
-            document.getElementById('myForms_employeeUpdateForm_employeeMobile').setAttribute('value','');
-            document.getElementById('myForms_employeeUpdateForm_employeeDesignation').setAttribute('value','');
-
-
+            commonFragment();
             document.getElementById("myParagraph_errorComment").innerHTML = '';
-            document.getElementById('myForms_employeeUpdateForm_employeeName').setAttribute('placeholder','Name');
-            document.getElementById('myForms_employeeUpdateForm_employeeEmail').setAttribute('placeholder','Email');
-            document.getElementById('myForms_employeeUpdateForm_departmentName').setAttribute('placeholder','Department/Team');
-            document.getElementById('myForms_employeeUpdateForm_departmentId').setAttribute('placeholder','Department/Team Id');
-            document.getElementById('myForms_employeeUpdateForm_joiningDate').setAttribute('placeholder','Date of Joining');
-            document.getElementById('myForms_employeeUpdateForm_employeeMobile').setAttribute('placeholder','Mobile');
-            document.getElementById('myForms_employeeUpdateForm_employeeDesignation').setAttribute('placeholder','employeeDesignation');
-        }
+          }
         else if (this.readyState == 4 && this.status == 200) {
             var obj=JSON.parse(this.responseText);
             var employee=obj[0];
@@ -46,26 +30,38 @@ function fetchData(stat) {
 
         }
         else{
-            document.getElementById('myForms_employeeUpdateForm_employeeName').setAttribute('value','');
-            //document.getElementById('myForms_employeeUpdateForm_employeeName').setAttribute('placeholder',employee.employeeName);
-            document.getElementById('myForms_employeeUpdateForm_employeeEmail').setAttribute('value','');
-            document.getElementById('myForms_employeeUpdateForm_departmentName').setAttribute('value','');
-            document.getElementById('myForms_employeeUpdateForm_departmentId').setAttribute('value','');
-            document.getElementById('myForms_employeeUpdateForm_joiningDate').setAttribute('value','');
-            document.getElementById('myForms_employeeUpdateForm_employeeMobile').setAttribute('value','');
-            document.getElementById('myForms_employeeUpdateForm_employeeDesignation').setAttribute('value','');
+            commonFragment();
 
-            document.getElementById("myParagraph_errorComment").innerHTML = 'Employee with id'+id+'does not exist';
-            document.getElementById('myForms_employeeUpdateForm_employeeName').setAttribute('placeholder','Name');
-            document.getElementById('myForms_employeeUpdateForm_employeeEmail').setAttribute('placeholder','Email');
-            document.getElementById('myForms_employeeUpdateForm_departmentName').setAttribute('placeholder','Department/Team');
-            document.getElementById('myForms_employeeUpdateForm_departmentId').setAttribute('placeholder','Department/Team Id');
-            document.getElementById('myForms_employeeUpdateForm_joiningDate').setAttribute('placeholder','Date of Joining');
-            document.getElementById('myForms_employeeUpdateForm_employeeMobile').setAttribute('placeholder','Mobile');
-            document.getElementById('myForms_employeeUpdateForm_employeeDesignation').setAttribute('placeholder','employeeDesignation');
+            if(stat==0) //means it is coming from employeeupdatejsp form else deleteemployeejsp form
+                document.getElementById("myParagraph_errorComment0").innerHTML ='Employee with id'+id+'does not exist';
+            else
+                document.getElementById("myParagraph_errorComment1").innerHTML ='Employee with id'+id+'does not exist';
 
         }
     };
     xhttp.open("GET", "/restemployees/"+id, true);
     xhttp.send();
+}
+
+function  commonFragment() {
+    //sets form field value to null and placeholder value to intended meaning
+    document.getElementById('myForms_employeeUpdateForm_employeeName').setAttribute('value','');
+    //document.getElementById('myForms_employeeUpdateForm_employeeName').setAttribute('placeholder',employee.employeeName);
+    document.getElementById('myForms_employeeUpdateForm_employeeEmail').setAttribute('value','');
+    document.getElementById('myForms_employeeUpdateForm_departmentName').setAttribute('value','');
+    document.getElementById('myForms_employeeUpdateForm_departmentId').setAttribute('value','');
+    document.getElementById('myForms_employeeUpdateForm_joiningDate').setAttribute('value','');
+    document.getElementById('myForms_employeeUpdateForm_employeeMobile').setAttribute('value','');
+    document.getElementById('myForms_employeeUpdateForm_employeeDesignation').setAttribute('value','');
+
+    document.getElementById('myForms_employeeUpdateForm_employeeName').setAttribute('placeholder','Name');
+    document.getElementById('myForms_employeeUpdateForm_employeeEmail').setAttribute('placeholder','Email');
+    document.getElementById('myForms_employeeUpdateForm_departmentName').setAttribute('placeholder','Department/Team');
+    document.getElementById('myForms_employeeUpdateForm_departmentId').setAttribute('placeholder','Department/Team Id');
+    document.getElementById('myForms_employeeUpdateForm_joiningDate').setAttribute('placeholder','Date of Joining');
+    document.getElementById('myForms_employeeUpdateForm_employeeMobile').setAttribute('placeholder','Mobile');
+    document.getElementById('myForms_employeeUpdateForm_employeeDesignation').setAttribute('placeholder','employeeDesignation');
+
+
+
 }
